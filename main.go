c151266246c29d6eea543a8563611d0861bffb2e
ТшÐNikkx92 <nikita.lodjanskiy@gmail.com>
2025-10-17 20:53:52 +0600

    1st

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type Kitreq struct {
+	CompanyId string `json:"CompanyId"`
+	UserLogin string `json:"UserLogin"`
+	Password  string `json:"Password"`
+	Date      string `json:"Date"`
+}
+
+type Kitresp struct {
+	Code int    `json:"code"`
+	Text string `json:"text"`
+}
+
+func main() {
+	r := gin.Default()
+	r.POST("/", func(c *gin.Context) {
+		var req Kitreq
+		if err := c.BindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		co, te := mainRequest(req.CompanyId, req.UserLogin, req.Password, req.Date)
+		c.JSON(http.StatusOK, Kitresp{Code: co, Text: te})
+	})
+	// слушает 0.0.0.0:8080
+	r.Run(":8080")
+}
